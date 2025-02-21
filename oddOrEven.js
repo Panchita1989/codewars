bdbeda1851756f77a444cbee92110c527abdbb03
Panchita <franciscalandwehr@hotmail.com>
2025-02-21 12:31:38 -0500

    new codingChallenge

---
+++ oddOrEven.js
@@ -0,0 +1,16 @@
+
+function oddOrEven(array) {
+    let sum = 0
+        for (let i = 0; i < array.length; i++){
+        sum += array[i]
+    };
+    console.log(sum)
+    if(sum % 2 === 0){
+      return "even"
+    }else{
+      return "odd"
+    };
+    
+ }
+
+console.log(oddOrEven([1, 2, 3, 4, 5]))
