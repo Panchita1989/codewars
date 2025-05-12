bdbff9a8ef8d56346c94555591982db31e42c711
Panchita <franciscalandwehr@hotmail.com>
2025-05-11 22:12:18 -0500

    new codingChallenge

---
+++ areYouPlayingBajo.js
@@ -0,0 +1,7 @@
+function areYouPlayingBajo(name) {
+    if(name[0].toLowerCase() === 'r'){
+        return name + ' is playing banjo'
+    }else{
+        return name + ' is not playing banjo'
+    }
+}
