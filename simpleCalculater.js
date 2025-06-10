ffeb0a35c23ad013a52d4dfb3ce80c74a0809fca
Panchita <franciscalandwehr@hotmail.com>
2025-06-09 22:13:08 -0500

    new codingChallenge

---
+++ simpleCalculater.js
@@ -0,0 +1,23 @@
+function calculator(a, b, sign){
+    if(typeof(a) === 'number' && typeof(b) === 'number'){
+        switch (sign) {
+            case '+':
+                return a + b 
+                break;
+            case '-':
+                return a - b 
+                break;
+            case '*':
+                return a * b 
+                break;  
+            case '/':
+                return a / b 
+                break;
+            default:
+                return 'unknown value'
+                break;
+        }
+    }else{
+        return 'unknown value'
+    }
+}
