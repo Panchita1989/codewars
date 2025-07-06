f163330c50b0c4213a95c571353ed99597f04f89
Panchita <franciscalandwehr@hotmail.com>
2025-07-05 21:39:14 -0500

    new codingchallenge

---
+++ uefaEuro.js
@@ -0,0 +1,8 @@
+function uefaEuro(teams, scores){
+    let winner = scores[0] > scores[1] ? teams[0] : teams[1]
+    if(scores[0] === scores[1]){
+        return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
+    }else{
+        return `At match ${teams[0]} - ${teams[1]}, ${winner} won!`
+    }
+}
