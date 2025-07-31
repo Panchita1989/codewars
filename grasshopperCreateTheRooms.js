47e1b713bb9ce48d90aa8262999a269cd6465273
Panchita <franciscalandwehr@hotmail.com>
2025-07-31 09:44:27 -0500

    new codingChallenge

---
+++ grasshopperCreateTheRooms.js
@@ -0,0 +1,18 @@
+const rooms = {
+  roomOne:{
+    name: 'One',
+    description: 'some kind of description',
+    completed: false
+  },
+  roomTwo:{
+    name: 'Two',
+    description:'another kind of description',
+    completed: false
+  },
+  roomThree:{
+    name:'three',
+    description:'last but not least',
+    completed: false
+  }
+
+}
