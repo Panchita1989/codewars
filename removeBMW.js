ce32e5805e6c100dcda3fa5a2292e820e18bdc7b
Panchita <franciscalandwehr@hotmail.com>
2025-10-03 15:54:18 -0500

    new codingChallenge

---
+++ removeBMW.js
@@ -0,0 +1,6 @@
+function removeBMW(str){
+    if(typeof(str) !== 'string'){
+        throw new Error("This program only works for text.");        
+    }
+    return str.replaceAll(/[bmw]/gi, '')
+}
