96e3ecc40596a11868c47eb13ab6ea7d901bb117
Panchita <franciscalandwehr@hotmail.com>
2025-10-12 20:11:28 -0500

    new codingChallenge

---
+++ grasshopperGradeBook.js
@@ -0,0 +1,10 @@
+function getGrade (s1, s2, s3) {
+  let score = (s1 + s2 + s3) / 3
+  
+  if(score >= 90 && score <= 100) return 'A'
+  if(score >= 80) return 'B'
+  if(score >= 70) return 'C'
+  if(score >= 60) return 'D'
+  
+  return 'F'
+}
